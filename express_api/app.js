f0fc42aa2e1d58009337986f3ae94900d0b3bfe7
Esmeralda Santoyo <58286718+EsmeSantoyo@users.noreply.github.com>
2022-05-01 16:34:33 -0500

    Add:  Probando tu Endpoint post

--- express_api/app.js
+++ express_api/app.js
@@ -24,6 +24,11 @@ app.get("/v1/explorers/:id", (req, res) => {
     res.status(200).json(explorer)
 })
 
+app.post("/v1/explorers", (req, res) => {
+    console.log(`Api explorers port request ${new Date()}`)
+    const requestBody = req.body
+    res.status(201).json({msg: "Created"})
+})
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
